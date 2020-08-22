4956c3dcf8dc7bd9015789d33b4140129c26f845
Charles Ivia <iviacharles@gmail.com>
2020-08-22 15:01:06 +0300

    Initial snapshot logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { db, auth } from "./Services/firebase";
 
 function App() {
+  const intitiaState = null;
+  const [students, setStudents] = useState(intitiaState);
+
+  useEffect(() => {
+    db.collection("students")
+      .get()
+      .then((snapshot) => {
+        const students = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          students.push(data);
+        });
+        console.log(students);
+        setStudents(students);
+      })
+      .catch((error) => console.log(error));
+  });
   return (
     <div className="App">
-      <h1>Hello World.</h1>
+      <h1>Xanadu High School Studemts</h1>
     </div>
   );
 }
